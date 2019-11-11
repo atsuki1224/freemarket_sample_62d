16e5c5629e8981b847a777b3d84fada4e202a7ca
atsuki1224 <akeve1224@icloud.com>
2019-11-11 18:24:07 +0900

    fixed-production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,6 +84,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  server '54.64.134.216', user: 'ec2-user', roles: %w{app db web}
 end
