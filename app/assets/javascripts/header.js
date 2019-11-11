439f828d3e9844ad5b6ff15aac7ca2c70d9c64fd
yumamitani <ymsy.w.life3@icloud.com>
2019-11-11 19:21:43 +0900

    icon link

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -9,4 +9,33 @@ $(function() {
 
   );
 
+
+
+  $('.header_brands').hover(function(){
+    $('.top_brand_list_btn').show();
+  },
+    function(){
+      $('.top_brand_list_btn').hide();
+    }
+
+  );
+
+  $('.header_news').hover(function(){
+    $('.top_news').show();
+  },
+    function(){
+      $('.top_news').hide();
+    }
+
+  );
+
+  $('.header_to_do_list').hover(function(){
+    $('.top_to_do_list').show();
+  },
+    function(){
+      $('.top_to_do_list').hide();
+    }
+
+  );
+
 });
