9e4fab8ea3fab23070f010bae7204dca44d031af
Yusuke2626 <54471437+Yusuke2626@users.noreply.github.com>
2019-11-20 22:21:43 +0900

    initial_commit

---
+++ db/migrate/20191120064952_change_data_trade_status_to_products.rb
@@ -0,0 +1,13 @@
+class ChangeDataTradeStatusToProducts < ActiveRecord::Migration[5.0]
+  def change
+
+    change_column :products,:trade_status,:integer
+    change_column :products,:size,:integer
+    change_column :products,:delivery_charge,:integer
+    change_column :products,:delivery_methot,:integer
+    change_column :products,:delivery_area,:integer
+    change_column :products,:delivery_time,:integer
+    rename_column :products, :methot ,:method
+
+  end
+end
