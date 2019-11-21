024d8bbda1bb884afd3f1f984a099fa193e78469
yumamitani <ymsy.w.life3@icloud.com>
2019-11-21 10:39:25 +0900

    define link color

--- app/assets/javascripts/product_detail.js
+++ app/assets/javascripts/product_detail.js
@@ -1,9 +1,9 @@
 $(function() {
-    $(".img").hover(function(){
-        let ImgSrc = $(this).attr("src");
-        $(".main-image").attr({src:ImgSrc});
-        $(".main-image").hide();
-        $(".main-image").fadeIn("slow");
-        return false;
-    });      
+  $(".img").hover(function(){
+      let ImgSrc = $(this).attr("src");
+      $(".main-image").attr({src:ImgSrc});
+      $(".main-image").hide();
+      $(".main-image").fadeIn("slow");
+      return false;
+  });      
 });
