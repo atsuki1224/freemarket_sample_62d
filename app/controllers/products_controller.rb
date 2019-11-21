bcd7bc1ce4207afeb156a0973b5d9748875140fd
yumamitani <ymsy.w.life3@icloud.com>
2019-11-21 16:03:47 +0900

    fix product_controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
+    # @images = Image.find_by(product_id:@product.id)
     @images = @product.images.order("created_at ASC").limit(10)
   end
 end
