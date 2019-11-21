8a09d932658871b4130e97ec0f8d4da29a4072a8
atsuki1224 <akeve1224@icloud.com>
2019-11-21 12:43:25 +0900

    maigrate-categpries-etc

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191118053540) do
+ActiveRecord::Schema.define(version: 20191120153927) do
+
+  create_table "blands", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["category_id"], name: "index_blands_on_category_id", using: :btree
+  end
+
+  create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "ancestry"
+    t.index ["ancestry"], name: "index_categories_on_ancestry", using: :btree
+  end
 
   create_table "images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "image",      null: false
@@ -23,14 +39,14 @@ ActiveRecord::Schema.define(version: 20191118053540) do
     t.string   "item_name",                     null: false
     t.text     "description",     limit: 65535, null: false
     t.integer  "item_condition",                null: false
-    t.string   "trade_status",                  null: false
-    t.string   "size"
+    t.integer  "trade_status",                  null: false
+    t.integer  "size"
     t.integer  "bland_id"
     t.integer  "category_id",                   null: false
-    t.string   "delivery_charge",               null: false
-    t.string   "delivery_methot",               null: false
-    t.string   "delivery_area",                 null: false
-    t.string   "delivery_time",                 null: false
+    t.integer  "delivery_charge",               null: false
+    t.integer  "delivery_method",               null: false
+    t.integer  "delivery_area",                 null: false
+    t.integer  "delivery_time",                 null: false
     t.integer  "price",                         null: false
     t.integer  "user_id",                       null: false
     t.datetime "created_at",                    null: false
