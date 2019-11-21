3e5e94c87cd91a4a544a470568da75c2e0cf9858
Tomato-98461 <aq10.tomato@gmail.com>
2019-11-21 13:40:50 +0900

    add newproduct-viewfiles

--- config/routes.rb
+++ config/routes.rb
@@ -28,6 +28,8 @@ Rails.application.routes.draw do
       end
     end
   end
+
+  resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root "homes#index"
