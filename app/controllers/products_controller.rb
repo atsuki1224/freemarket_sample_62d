9b35509a5144db5adac4552cddfd3c661f674ad2
yumamitani <ymsy.w.life3@icloud.com>
2019-11-23 18:21:57 +0900

    require user table

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,22 @@
 class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
-    # @images = Image.find_by(product_id:@product.id)
     @images = @product.images.order("created_at ASC").limit(10)
+
+    @grand_child_category = @product.category
+    @child_category = @grand_child_category.parent
+    @category = @grand_child_category.root
+
+    @bland = @product.bland
+
+    if @bland
+      @similar_items = Product.where.not(
+        id: params[:id]).where(
+          category_id: @grand_child_category.id, bland_id: @bland.id).limit(6)
+    else
+      @similar_items = Product.where.not(
+        id: params[:id]).where(
+          category_id: @grand_child_category).limit(6)
+    end
   end
 end
