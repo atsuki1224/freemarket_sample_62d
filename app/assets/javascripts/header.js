5ea47576d1404566e85cfea616bc757290907023
Yusuke2626 <54471437+Yusuke2626@users.noreply.github.com>
2019-11-26 18:50:13 +0900

    Update header.js

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -26,4 +26,11 @@ $(function() {
     function(){
       $('.todo-box').hide();
     });
+    $(".img").hover(function(){
+        let ImgSrc = $(this).attr("src");
+        $(".main-image").attr({src:ImgSrc});
+        $(".main-image").hide();
+        $(".main-image").fadeIn("slow");
+        return false;
+    });
 });
