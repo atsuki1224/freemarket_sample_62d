655ae65c5ba97e0dba10476d58464b83aa4c9069
Yusuke2626 <54471437+Yusuke2626@users.noreply.github.com>
2019-11-26 13:01:37 +0900

    add_comitt

--- app/models/bland.rb
+++ app/models/bland.rb
@@ -1,7 +1,7 @@
 class Bland < ApplicationRecord
 
   has_many:products
-  belongs_to:categories,optional:true
+  belongs_to:category,optional:true
 
   scope :search_name, ->(name){ where('name LIKE ?',name)}
 
