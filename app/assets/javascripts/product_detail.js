902091d5f15727870e0757c6176d3110817e12a4
Yusuke2626 <54471437+Yusuke2626@users.noreply.github.com>
2019-11-26 18:51:32 +0900

    Create product_detail.js

---
+++ app/assets/javascripts/product_detail.js
@@ -0,0 +1,10 @@
+$(function() {
+  $(".img").hover(function(){
+      let ImgSrc = $(this).attr("src");
+      $(".main-image").attr({src:ImgSrc});
+      $(".main-image").hide();
+      $(".main-image").fadeIn("slow");
+      return false;
+  });
+  
+});
