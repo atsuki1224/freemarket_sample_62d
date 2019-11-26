67c0a0a620cf2dd6d366cff523324f7181db2892
atsuki1224 <akeve1224@icloud.com>
2019-11-26 19:55:46 +0900

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
-
 ActiveRecord::Schema.define(version: 20191122011259) do
 
   create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -25,8 +22,6 @@ ActiveRecord::Schema.define(version: 20191122011259) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
   end
-
-
 
   create_table "blands", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
@@ -70,8 +65,6 @@ ActiveRecord::Schema.define(version: 20191122011259) do
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-
-
     t.string   "first_name",                                        null: false
     t.string   "first_name_kana",                                   null: false
     t.string   "last_name",                                         null: false
@@ -93,7 +86,6 @@ ActiveRecord::Schema.define(version: 20191122011259) do
     t.datetime "updated_at",                                        null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-
   end
 
 end
