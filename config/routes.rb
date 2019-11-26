62261cbf529e1bbd386d11ec157bcaedb7c2fecc
Yusuke2626 <54471437+Yusuke2626@users.noreply.github.com>
2019-11-26 13:10:12 +0900

    add_comitt

--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,8 @@ Rails.application.routes.draw do
       end
     end
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  resources :products, except: :index
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "homes#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191120134309) do
+ActiveRecord::Schema.define(version: 20191125083512) do
 
   create_table "blands", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
@@ -51,6 +51,12 @@ ActiveRecord::Schema.define(version: 20191120134309) do
     t.integer  "user_id",                       null: false
     t.datetime "created_at",                    null: false
     t.datetime "updated_at",                    null: false
+  end
+
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
