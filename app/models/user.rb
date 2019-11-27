8a08372d4f956a986d722681cc7b87a2aab3a422
Tomato-98461 <aq10.tomato@gmail.com>
2019-11-27 12:32:58 +0900

    edit Model-association

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # has_many :porducts
+  # has_many :nices
+  # has_many :commnets
+  # has_many :messages
+  # has_many :buyer_userReview, class_name:”user_review”, foreign_key: :buyer_id
+  # has_many :buyers, through: :buyer_userReview, source: :seller
+  # has_many :seller_userReview, class_name:”user_review”, foreign_key: :seller_id
+  # has_many :sellers, through: :seller_userReviews, source: :buyer
+  has_one :card
+  accepts_nested_attributes_for :card
+  has_one :address
+  accepts_nested_attributes_for :address
 end
