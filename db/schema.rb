43a0624d17fa5da5608e9d63c03f153c2350e870
Tomato-98461 <aq10.tomato@gmail.com>
2019-11-27 12:29:55 +0900

    addressTable addcolumn(zipcode)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191127025004) do
+ActiveRecord::Schema.define(version: 20191127032844) do
 
   create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "prefecture_id", null: false
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20191127025004) do
     t.integer  "user_id",       null: false
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.string   "zipcode",       null: false
   end
 
   create_table "blands", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
