5bbcc383f20da94e1773bad011e0b8518381e865
atsuki1224 <akeve1224@icloud.com>
2019-11-28 12:40:45 +0900

    edit carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -14,9 +14,9 @@ CarrierWave.configure do |config|
     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
     region: 'ap-northeast-1'
     
+    config.fog_directory  = 'freemarket62d'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket62d'
+  
+    end
   }
-  config.fog_directory  = 'freemarket62d'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket62d'
-
-  end
 end
