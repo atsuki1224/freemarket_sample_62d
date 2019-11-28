01df467507ad1f745a263ed55319d95d9b6f2acd
atsuki1224 <akeve1224@icloud.com>
2019-11-28 11:49:49 +0900

    change column name methot->method

--- db/migrate/20191120064952_change_data_trade_status_to_products.rb
+++ db/migrate/20191120064952_change_data_trade_status_to_products.rb
@@ -4,7 +4,7 @@ class ChangeDataTradeStatusToProducts < ActiveRecord::Migration[5.0]
     change_column :products,:trade_status,:integer
     change_column :products,:size,:integer
     change_column :products,:delivery_charge,:integer
-    change_column :products,:delivery_methot,:integer
+    change_column :products,:delivery_method,:integer
     change_column :products,:delivery_area,:integer
     change_column :products,:delivery_time,:integer
     # rename_column :products, :delivery_methot ,:delivery_method
