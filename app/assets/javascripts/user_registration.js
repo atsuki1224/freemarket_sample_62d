9aff964bc6d9501e2075614642333ddb1d31d621
Yusuke2626 <54471437+Yusuke2626@users.noreply.github.com>
2019-11-29 17:20:11 +0900

    add

--- app/assets/javascripts/user_registration.js
+++ app/assets/javascripts/user_registration.js
@@ -2,7 +2,7 @@ $(function(){
   // console.log('hello');
   $('#check').change(function(){
     if($(this).prop('checked')){
-      // console.log('hello');
+
       $('#password').attr('type','text');
     }else{
       $('#password').attr('type','password');
