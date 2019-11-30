3a1053a4c701229e413e6ec8c86648b871b1ca5b
Tomato-98461 <aq10.tomato@gmail.com>
2019-11-30 16:19:52 +0900

    add_user-profile-update

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -28,10 +28,34 @@ class MypageController < ApplicationController
   end
 
   def profile
+    @user = User.find(current_user.id)
+  end
+
+  def profile_update
+    @user = User.find(current_user.id)
+    if update_resource(@user, user_params)
+      # 出来た → 再表示して変更成功メッセージ
+      redirect_to mypage_index_path
+    else
+      # 出来なかった → 再表示してエラーメッセージ
+      render :profile
+    end
   end
 
   private
   def user_signed_in_check
     redirect_to user_session_path unless user_signed_in?
+  end
+
+  def user_params
+    params.require(:user).permit(
+      :nickname,
+      :user_profile
+    )
+  end
+
+  protected
+  def update_resource(resource, params)
+    resource.update_without_current_password(params)
   end
 end
