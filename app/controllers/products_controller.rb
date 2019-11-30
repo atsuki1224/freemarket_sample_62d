ea1535c46b6e419015c5d48f414694135955593a
atsuki1224 <akeve1224@icloud.com>
2019-11-30 18:28:58 +0900

    add iages build

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -60,6 +60,38 @@ class ProductsController < ApplicationController
   
   def edit
     @products = Product.find(params[:id])
+    @products.images.build
+
+
+    #//////カテゴリ、ブランド用////////
+      @parents = Category.sort_parents
+      @parent = Category.find_by(id:params[:category_id])
+      @child = Category.find_by(id:params[:child_id])
+      @search = params[:bland_name]
+  
+  
+  
+      if @parent && @child && @search
+          @blands1 = @parent.blands
+          @blands2 = @child.blands
+  
+        if @blands2.empty?
+            @blands = @blands1.search_name(params[:bland_name])
+        else
+            @blands = @blands2.search_name(params[:bland_name])
+        end
+  
+      elsif @parent && @child ==nil && @search==nil
+        @children = @parent.children
+  
+      elsif @child && @parent ==nil && @search==nil
+        @grand_children = @child.children
+      end
+  
+      respond_to do |format|
+        format.html
+        format.json
+      end
   end
 
   def update
