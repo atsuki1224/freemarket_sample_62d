80073e90771e3df99ff98b960291a8d81c4648b1
Tomato-98461 <aq10.tomato@gmail.com>
2019-11-30 18:24:34 +0900

    edit_user-address(getDB)

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -1,5 +1,7 @@
 class MypageController < ApplicationController
   before_action :user_signed_in_check
+  before_action :user_selected
+
   def index
   end
 
@@ -24,25 +26,33 @@ class MypageController < ApplicationController
   def identification
   end
 
+  def identification_update
+    if @user.address.update(user_params[:address_attributes])
+      redirect_to mypage_index_path
+    else
+      render :notification
+    end
+  end
+
   def edit
   end
 
   def profile
-    @user = User.find(current_user.id)
   end
 
   def profile_update
-    @user = User.find(current_user.id)
     if update_resource(@user, user_params)
-      # 出来た → 再表示して変更成功メッセージ
       redirect_to mypage_index_path
     else
-      # 出来なかった → 再表示してエラーメッセージ
       render :profile
     end
   end
 
   private
+  def user_selected
+    @user = User.find(current_user.id)
+  end
+
   def user_signed_in_check
     redirect_to user_session_path unless user_signed_in?
   end
@@ -50,8 +60,10 @@ class MypageController < ApplicationController
   def user_params
     params.require(:user).permit(
       :nickname,
-      :user_profile
-    )
+      :user_profile,
+      address_attributes: [:zipcode, :prefecture_id, :city, :address, :phone_number],
+      card_attributes: [:card_number, :validated_data, :security_code]
+    ).merge(user_id: current_user.id)
   end
 
   protected
