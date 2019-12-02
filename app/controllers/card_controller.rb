c18e0542da5ddda803a82757a29a9d77984ed323
yamazakitakehiro <ndabadaaaaaa@yahoo.co.jp>
2019-12-02 16:57:05 +0900

    Update card_controller.rb

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -5,19 +5,18 @@ class CardController < ApplicationController
     product = Product.find(params[:product_id])
     Trade.create(user_id:1, product_id: params[:product_id])
     product.update(trade_status:1)
-
+    if 
+      Trade.create(user_id:params[:user_id], product_id: params[:product_id]) && product.update(trade_status:1) 
+      redirect_to root_path
+    else
+      confirmation_product_path(@product)
+    end
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     charge = Payjp::Charge.create(
     :amount => product.price,
     :card => params['payjp-token'],
     :currency => 'jpy',
   )
-  if 
-    Trade.create(user_id:params[:user_id], product_id: params[:product_id]) && product.update(trade_status:1) 
-    redirect_to root_path
-  else
-    confirmation_product_path(@product)
-  end
 
   end
 
