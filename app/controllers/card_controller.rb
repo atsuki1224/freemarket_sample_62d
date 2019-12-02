745fc7710b9df53417a13b2e5448df049f17242d
yamazakitakehiro <ndabadaaaaaa@yahoo.co.jp>
2019-12-02 17:21:07 +0900

    Update card_controller.rb

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -7,7 +7,7 @@ class CardController < ApplicationController
     Trade.create(user_id:current_user.id, product_id: params[:product_id]) && product.update(trade_status:1) 
     redirect_to root_path
   else
-    confirmation_product_path(@product)
+    confirmation_product_path(product)
   end
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     charge = Payjp::Charge.create(
