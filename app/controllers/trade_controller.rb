a71f3f9ef4536a258851eb440dda4328811b3b9d
yamazakitakehiro <ndabadaaaaaa@yahoo.co.jp>
2019-12-02 15:37:42 +0900

    delete controller

--- app/controllers/trade_controller.rb
+++
@@ -1,9 +0,0 @@
-class TradeController < ApplicationController
-
-  def create
-    product = Product.find(params[:product_id])
-    Trade.create(user_id:1, product_id: params[:product_id])
-    product.update(trade_status:1)
-    redirect_to root_path
-  end
-end
