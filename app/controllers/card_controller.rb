478b32e1f8b98a7b85d2dbdb1a5cd105fbedee7d
yamazakitakehiro <ndabadaaaaaa@yahoo.co.jp>
2019-12-02 17:08:46 +0900

    Update card_controller.rb

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -3,14 +3,12 @@ class CardController < ApplicationController
 
   def pay
     product = Product.find(params[:product_id])
-    Trade.create(user_id:1, product_id: params[:product_id])
-    product.update(trade_status:1)
     if 
-      Trade.create(user_id:params[:user_id], product_id: params[:product_id]) && product.update(trade_status:1) 
-      redirect_to root_path
-    else
-      confirmation_product_path(@product)
-    end
+    Trade.create(user_id:current_user.id, product_id: params[:product_id]) && product.update(trade_status:1) 
+    redirect_to root_path
+  else
+    confirmation_product_path(@product)
+  end
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     charge = Payjp::Charge.create(
     :amount => product.price,
