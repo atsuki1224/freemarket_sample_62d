aa9037b7703f018c542ce116bb868ad987ee534a
YUSUKE MATSUMURA <54471437+Yusuke2626@users.noreply.github.com>
2019-12-04 08:59:55 +0900

    Update user_registration.js

--- app/assets/javascripts/user_registration.js
+++ app/assets/javascripts/user_registration.js
@@ -1,11 +1,9 @@
 $(function(){
-
   $('#check').change(function(){
     if($(this).prop('checked')){
       $('#user_password').attr('type','text');
     } else {
       $('#user_password').attr('type','password');
-
     }
   });
 });
