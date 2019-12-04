65a6d16edadd29870cec9e05346288db7473c554
YUSUKE MATSUMURA <54471437+Yusuke2626@users.noreply.github.com>
2019-12-04 13:00:23 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -75,8 +75,7 @@ Rails.application.routes.draw do
       get 'search'
     end
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+ 
 
   resources :products, except: :index
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
