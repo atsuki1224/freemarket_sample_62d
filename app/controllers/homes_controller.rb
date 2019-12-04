fc111df13098eb82a052cf7efd045f7c94bd55d5
YUSUKE MATSUMURA <54471437+Yusuke2626@users.noreply.github.com>
2019-12-04 09:00:38 +0900

    Update homes_controller.rb

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -6,7 +6,6 @@ class HomesController < ApplicationController
     @home_appliance_products = Product.where(category_id: 954..1000).order("created_at DESC").limit(10)
     @toy_products = Product.where(category_id: 765..865).order("created_at DESC").limit(10)
 
-
     @chanel_products = Product.where(bland_id: 2447).order("created_at DESC").limit(10)
     @louis_vuitton_products = Product.where(bland_id: 6155).order("created_at DESC").limit(10)
     @supreme_products = Product.where(bland_id: 8413).order("created_at DESC").limit(10)
