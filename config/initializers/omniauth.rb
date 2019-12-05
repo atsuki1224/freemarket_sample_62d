e285c8161365c8c053e076887dd13d58fb503c2e
atsuki1224 <akeve1224@icloud.com>
2019-12-05 14:47:56 +0900

    add facebookauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2,Rails.application.secrets.google_client_id,Rails.application.secrets.google_client_secret,skip_jwt: true
+  provider :facebook,Rails.application.secrets.facebook_client_id,Rails.application.secrets.facebook_client_secret,skip_jwt: true
+
 end
