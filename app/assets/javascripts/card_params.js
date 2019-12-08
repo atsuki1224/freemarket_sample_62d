af47d30c91f6cc7cf5ea3e5369021e8e1818f2a7
Tomato-98461 <aq10.tomato@gmail.com>
2019-12-08 19:19:03 +0900

    card_params.js_delComment

--- app/assets/javascripts/card_params.js
+++ app/assets/javascripts/card_params.js
@@ -1,28 +1,28 @@
 document.addEventListener(
   "DOMContentLoaded", e => {
-    if (document.getElementById("token_submit") != null) { //token_submitというidがnullの場合、下記コードを実行しない
-      Payjp.setPublicKey("pk_test_cd3c1d3f0420b3bc72b91a90"); //ここに公開鍵を直書き
-      let btn = document.getElementById("token_submit"); //IDがtoken_submitの場合に取得されます
-      btn.addEventListener("click", e => { //ボタンが押されたときに作動します
-        e.preventDefault(); //ボタンを一旦無効化します
+    if (document.getElementById("token_submit") != null) {
+      Payjp.setPublicKey("pk_test_cd3c1d3f0420b3bc72b91a90");
+      let btn = document.getElementById("token_submit");
+      btn.addEventListener("click", e => {
+        e.preventDefault();
         let card = {
           number: document.getElementById("card_number").value,
           cvc: document.getElementById("security_code").value,
           exp_month: document.getElementById("exp_month").value,
           exp_year: document.getElementById("exp_year").value
-        }; //入力されたデータを取得します。
+        };
         Payjp.createToken(card, (status, response) => {
-          if (status === 200) { //成功した場合
+          if (status === 200) {
             $("#card_number").removeAttr("name");
             $("#security_code").removeAttr("name");
             $("#exp_month").removeAttr("name");
-            $("#exp_year").removeAttr("name"); //データを自サーバにpostしないように削除
+            $("#exp_year").removeAttr("name");
             $("#card_token").append(
               $('<input type="hidden" name="payjp-token">').val(response.id)
-            ); //取得したトークンを送信できる状態にします
+            );
             document.inputForm.submit();
           } else {
-            alert("カード情報が正しくありません。"); //確認用
+            alert("カード情報が正しくありません。");
           }
         });
       });
